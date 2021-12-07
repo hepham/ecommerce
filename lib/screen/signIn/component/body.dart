984975ff1f479ce5ec95efb27bdb78065fbb47ba
Pein Hatake <haxuandang0112@gmail.com>
2021-12-07 17:30:01 +0700

    change avatar and login update

--- lib/screen/signIn/component/body.dart
+++ lib/screen/signIn/component/body.dart
@@ -1,141 +1,62 @@
-import 'package:commerce/config.dart';
-import 'package:commerce/screen/Home/homeScreen.dart';
-import 'package:commerce/screen/signIn/component/forgotpassword.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:commerce/component/socalcard.dart';
+import 'package:commerce/screen/signIn/component/signform.dart';
 import 'package:flutter/material.dart';
 
-bool obserText1 = true;
+import '../../../config.dart';
+import 'no_account_text.dart';
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: SizedBox(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Spacer(),
-          Text(
-            'Welcome Back',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: getProportionateScreenWidth(28),
-            ),
-          ),
-          Text(
-            'Sign in to your account',
-            textAlign: TextAlign.center,
-          ),
-          SignForm(),
-          SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(20)),
-            child: SizedBox(
-              width: double.infinity,
-              height: getProportionateScreenHeight(56),
-              child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, homeScreen.routeName);
-                  },
-                  color: Colors.redAccent,
-                  child: Text(
-                    "Continue",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: getProportionateScreenWidth(18),
-                    ),
-                  )),
-            ),
-          ),
-          Spacer(),
-          Spacer(),
-        ],
-      ),
-    ));
-  }
-}
-
-class SignForm extends StatefulWidget {
-  const SignForm({Key? key}) : super(key: key);
-
-  @override
-  _SignFormState createState() => _SignFormState();
-}
-
-class _SignFormState extends State<SignForm> {
-  bool remember = false;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
+      child: SizedBox(
+        width: double.infinity,
         child: Padding(
-      padding:
+          padding:
           EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "Enter your email or phone number",
-              labelText: "Email",
-            ),
-          ),
-          TextFormField(
-            obscureText: obserText1,
-            decoration: InputDecoration(
-              hintText: "Enter your password",
-              labelText: "Password",
-              suffixIcon: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    obserText1 = !obserText1;
-                  });
-                  FocusScope.of(context).unfocus();
-                },
-                child: Icon(
-                  obserText1 == true ? Icons.visibility : Icons.visibility_off,
-                  color: Colors.black,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
+                Text(
+                  "Sign in with your email and password  \nor continue with social media",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocalCard(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
+                    ),
+                    SocalCard(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocalCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                NoAccountText(),
+              ],
             ),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: [
-              Checkbox(
-                  value: remember,
-                  activeColor: Colors.redAccent,
-                  onChanged: (value) {
-                    setState(() {
-                      remember = value!;
-                    });
-                  }),
-              Text("Remember me"),
-              Spacer(),
-              GestureDetector(
-                onTap: (){
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (ctx) => ForgotPassword(),
-                    ),
-                  );
-
-                },
-                child: Text("Forgot Password ?",style: TextStyle(decoration: TextDecoration.underline),),
-              ),
-
-            ],
-          ),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
