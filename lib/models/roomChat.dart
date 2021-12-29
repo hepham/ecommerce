7781dbe7daae3db4946a635c78a7267b13d1f6d4
Dũng Uông <contennao2000@gmail.com>
2021-12-29 23:03:03 +0700

    'chat4'

--- lib/models/roomChat.dart
+++ lib/models/roomChat.dart
@@ -4,20 +4,23 @@ import 'package:commerce/models/messages.dart';
 
 class Room {
   final int roomid;
-  final Data user;
+  final Data user1;
+  final Data user2;
   final String mess;
 
   Room(
     this.roomid,
-    this.user,
+    this.user1,
+    this.user2,
     this.mess,
   );
 }
 
-String a= messages[1].text;
+// dung cai nay thi bi loi String a = messages[messages.length].text;
+String a= messages[4].text;
 
 List<Room> roomchat = [
-  Room(1, user1, a ),
+  Room(1, user1, user, a ),
 
 
 ];
