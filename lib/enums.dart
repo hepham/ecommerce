711850ca9cc28718e60b3b728d37d73f811963e3
Pein Hatake <haxuandang0112@gmail.com>
2022-01-09 04:35:37 +0700

    fix

--- lib/enums.dart
+++ lib/enums.dart
@@ -1,3 +1,3 @@
 enum MenuState { home, message,cart,sell, profile,none, }
-String ApiUrl='https://e0ed-2405-4803-a0bf-36b0-7061-17a2-a724-e6b7.ngrok.io/api';
-String ApiUrlChatRealTime='https://e0ed-2405-4803-a0bf-36b0-7061-17a2-a724-e6b7.ngrok.io/chat';
+String ApiUrl='https://ecommerce.dungpham.com.vn/api';
+String ApiUrlChatRealTime='https://ecommerce.dungpham.com.vn/chat';
